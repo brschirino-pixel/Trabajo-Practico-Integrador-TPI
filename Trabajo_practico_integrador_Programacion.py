784a3a8ea320157580ca22c834ca9ab2f62dc088
clarisaboffelli <clarisaboffelli@gmail.com>
2026-05-10 12:19:57 -0300

    Definicion inicial de elementos
    Se definen las listas, funciones y variables iniciales del programa.

---
+++ Trabajo_practico_integrador_Programacion.py
@@ -0,0 +1,45 @@
+# Listas:
+
+nombre_lista = []
+poblacion_lista = []
+continente_lista = []
+superficie_lista = []
+
+# Funciones:
+
+# Funciones de validación de datos:
+
+def validar_texto(texto):
+    while not (texto.isalpha() and texto != "" and len(texto) <= 50):
+        print("El valor ingresado no es un texto válido. Por favor, ingrese solo letras.")
+        texto = input(f"Ingrese el {texto} nuevamente: ").strip()
+
+def validar_numero(numero):
+    while not (numero.isdigit() and numero != "" and int(numero) > 0):
+        print("El valor ingresado no es un número válido. Por favor, ingrese un número entero positivo.")
+        numero = input(f"Ingrese el {numero} nuevamente: ").strip()
+
+# Funciones de ingreso de datos:
+def pais(nombre,poblacion,superficie,continente):
+    nombre = input("Ingrese el nombre del país: ").strip().capitalize()
+    validar_texto(nombre)
+    nombre_lista.append(nombre)
+    poblacion = input("Ingrese la población del país: ").strip()
+    validar_numero(poblacion)
+    poblacion_lista.append(poblacion)
+    superficie = input("Ingrese la superficie del país en km²: ").strip()
+    validar_numero(superficie)
+    superficie_lista.append(superficie)
+    continente = input("Ingrese el continente al que pertenece el país: ").strip().capitalize() 
+    validar_texto(continente)
+    continente_lista.append(continente)
+    return nombre, poblacion, superficie, continente
+    
+# Listas:
+nombre_lista = []
+poblacion_lista = []
+continente_lista = []
+superficie_lista = []
+
+
+
