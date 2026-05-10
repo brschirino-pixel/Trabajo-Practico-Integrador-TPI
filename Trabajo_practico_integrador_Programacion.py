32f5abe0f19d0c8606599889102c5211a44d8490
clarisaboffelli <clarisaboffelli@gmail.com>
2026-05-10 13:17:36 -0300

    Creacion de variables globales
    Se crean la variables fuera de la definicion de la funcion. Evaluar si la definicion de la funcion no es muy extensa.

--- Trabajo_practico_integrador_Programacion.py
+++ Trabajo_practico_integrador_Programacion.py
@@ -5,6 +5,12 @@ poblacion_lista = []
 continente_lista = []
 superficie_lista = []
 
+# Variables globales:
+nombre = ""
+poblacion = 0   
+superficie = 0
+continente = ""
+
 # Funciones:
 
 # Funciones de validación de datos:
@@ -14,13 +20,13 @@ def validar_texto(texto):
         print("El valor ingresado no es un texto válido. Por favor, ingrese solo letras.")
         texto = input(f"Ingrese el {texto} nuevamente: ").strip()
 
-def validar_numero(numero):
-    while not (numero.isdigit() and numero != "" and int(numero) > 0):
+def validar_numero(cantidad):
+    while not (cantidad.isdigit() and cantidad != "" and int(cantidad) > 0):
         print("El valor ingresado no es un número válido. Por favor, ingrese un número entero positivo.")
-        numero = input(f"Ingrese el {numero} nuevamente: ").strip()
+        cantidad = input(f"Ingrese la {cantidad} nuevamente: ").strip()
 
 # Funciones de ingreso de datos:
-def pais(nombre,poblacion,superficie,continente):
+def pais(nombre: str, poblacion: int, superficie: int, continente: str):
     nombre = input("Ingrese el nombre del país: ").strip().capitalize()
     validar_texto(nombre)
     nombre_lista.append(nombre)
